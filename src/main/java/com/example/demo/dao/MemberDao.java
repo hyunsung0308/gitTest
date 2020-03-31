ce4d2b476f96a4ba975fdd882513f854f1a5883d
0hmuse <88.hmuse@gmail.com>
2020-03-31 15:28:28 +0900

    지연다시올림

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -13,7 +13,7 @@ public class MemberDao {
 	public void updateMember() {
 		System.out.println("회원의 정보를 수정했습니다.");
 	}
-	public void deleteMember() {
+	public void deleteMember2() {
 		System.out.println("회원의 정보를 삭제하였습니다.");
 	}
 }
