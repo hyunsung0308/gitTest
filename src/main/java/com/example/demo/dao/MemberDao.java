27606ff7887188289da27b64ebe34dfb5bc6e429
hyunsung0308 <hyunsung0308@gmail.com>
2020-03-31 15:27:41 +0900

    현성

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -2,16 +2,10 @@ package com.example.demo.dao;
 
 public class MemberDao {
 
-	public void login() {
-		System.out.println("메롱");
-	}
 	
-	public void insertMember() {
-	System.out.println("하이!!");	
-	}
 	
-	public void insertMember2() {
+	public void listMember2() {
 		System.out.println("하이!!");	
-		}
+	}
 		
 }
