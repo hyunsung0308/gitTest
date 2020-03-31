b6da49dfc9be86a53bd5efdb0d5727f33d3ee7f4
jackdopark <jackdopark8@gmail.com>
2020-03-31 14:57:17 +0900

    deleteMember까지 완성.

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -4,9 +4,13 @@ public class MemberDao {
 
 	
 	public void insertMember() {
-	System.out.println("하이!!");	
+	System.out.println("안녕");	
 	}
+	
 	public void updateMember() {
-		System.out.println("회원의 정보를 삭제하였습니다");
+		System.out.println("회원의 정보를 수정했습니다.");
+	}
+	public void deleteMember() {
+		System.out.println("회원의 정보를 삭제하였습니다.");
 	}
 }
